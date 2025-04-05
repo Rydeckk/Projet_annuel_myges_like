05e7b1a88fb47fe90630b64e36d332fc75678655
Nicolas9000 <92756075+Nicolas9000@users.noreply.github.com>
2025-04-05 08:46:08 +0200

    improvement: apply linter + prettier & remove husky for the moment

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    credentials: true,
-  } as CorsOptions);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        credentials: true,
+    } as CorsOptions);
+    await app.listen(process.env.PORT ?? 3000);
 };
 
 void bootstrap();
