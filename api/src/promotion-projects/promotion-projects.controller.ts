31eebbc146c99cde77faab56f05cec7c3830562f
Nicolas9000 <92756075+Nicolas9000@users.noreply.github.com>
2025-06-19 15:10:34 +0200

    feat: add get student promotion project

--- api/src/promotion-projects/promotion-projects.controller.ts
+++ api/src/promotion-projects/promotion-projects.controller.ts
@@ -1,13 +1,28 @@
-import { Body, Controller, Post, SerializeOptions } from "@nestjs/common";
+import { Body, Controller, Get, Post, SerializeOptions } from "@nestjs/common";
 import { PromotionProjectsService } from "./promotion-projects.service";
 import { CreatePromotionProjectDto } from "./dto/promotion-project.dto";
 import { PromotionProjectEntity } from "./entities/promotion-project.entity";
+import { GetCurrentUser } from "decorators/user.decorator";
 
 @Controller("promotion-projects")
 export class PromotionProjectsController {
     constructor(
         private readonly promotionProjectsService: PromotionProjectsService,
     ) {}
+
+    @Get("student")
+    @SerializeOptions({ type: PromotionProjectEntity })
+    async findStudentProjects(@GetCurrentUser("id") userScopeId: string) {
+        return this.promotionProjectsService.findMany({
+            promotion: {
+                promotionStudents: {
+                    some: {
+                        studentId: userScopeId,
+                    },
+                },
+            },
+        });
+    }
 
     @Post()
     @SerializeOptions({ type: PromotionProjectEntity })
